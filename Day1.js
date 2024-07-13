57026b247343d42970e94b57bdfb69933ec2f0dd
priyanshusoni <priyanshusoni0927@gmail.com>
2024-07-13 20:26:14 +0530

    day 1 completed

---
+++ Day1.js
@@ -0,0 +1,57 @@
+//Activity 1
+
+// Variable Declaration 
+
+
+var  a =10;
+console.log(a);
+
+var b = 'Mynames';
+console.log(b);
+
+
+
+//Activity 2
+
+const bool = true;
+
+console.log(bool);
+
+
+//Activity 3
+
+let number = 10;
+let string = 'Mynames';
+let boolean = true;
+let object = { a:1, b:2};
+
+let array= [1,2,3,4,5];
+
+
+console.log(typeof array); // Note: In JavaScript, arrays are considered objects, so the typeof operator returns "object" for arrays.
+console.log(typeof object);
+console.log(typeof boolean);
+console.log(typeof string);
+console.log(typeof number);
+
+
+
+//Activity 4
+
+let firstname = 'John';
+console.log(firstname);  // john
+
+firstname='Ram';
+console.log(firstname);  //Ram
+
+
+
+//Activity 5
+
+const value =20;
+
+value =30;
+
+
+console.log(value);
+
