78d4976783145dc4cb4ca334a6503816f54ca41b
priyanshusoni <priyanshusoni0927@gmail.com>
2024-07-14 16:50:49 +0530

    Day 2 completed

---
+++ Day2.js
@@ -0,0 +1,105 @@
+/*   Activity 1  */ 
+
+let number1 =100;
+let number2 =30;
+
+let sum = number1 + number2;
+
+console.log("Sum :-" + sum);
+
+
+let substraction = number1 - number2;
+
+console.log("Substraction :-  " + substraction);
+
+
+let multiplication = number1 * number2;
+
+console.log("Multiplication :-" + multiplication);
+
+
+let division = number1 / number2;
+
+console.log(number1 / number2);
+
+const num =3;
+num>0?console.log("Positive Number"):console.log("Negative Number");
+
+
+
+let upper = 23;
+
+let lower = 7;
+
+console.log(`Remainder is : ${upper%lower}`);
+
+/*   Activity 2  */
+
+let a = 10;
+a +=10;
+
+console.log(a);
+
+a-= 20;
+
+console.log(a);
+
+
+
+/* Activity 3 */
+
+
+
+
+
+
+const num1 =1;
+const num2 =0;
+
+if(num1>num2){
+    console.log("Num1 is greater than num2");
+}
+else if(num2 > num1){
+    console.log(" num 2 is greater than num1");
+}
+
+
+
+
+
+let first = 12;
+
+let second = "12";
+
+console.log(first==second);
+
+console.log(first===second); // datatype check 
+
+
+
+/* Activity 4 */
+
+let username = "abc";
+
+let userid = 12543;
+
+if(username=="abc" && userid==12543){
+    console.log("You are allowed to sign up");
+}
+
+if (username=="abc" || userid== 12543) {
+        console.log("You are allowed to sign up");
+}
+
+
+const bool = true;
+
+console.log(`! of bool value is  ${!bool}`);
+
+
+/*Activity 5 */
+
+const value =3;
+value>0?console.log("Positive Number"):console.log("Negative Number");
+
+
